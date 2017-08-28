362d80fafb1bb8f047acdb2a07562f1b072ad350
bvjorge <jorgenes1@hotmail.com>
2017-08-27 23:52:46 -0300

    projeto tcc

---
+++ src/app/prevtrans-admin/services/cep.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import {Headers, Http, RequestOptions } from '@angular/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import {Observable} from 'rxjs/Observable';
+import {CEP_API} from '../../app.api';
+import {PrevtransAdminHerrorHandler} from '../prevtrans-admin-herror-handler';
+import {Cep} from '../models';
+
+@Injectable()
+export class CepService {
+
+  constructor(private http: Http) { }
+
+  consultaCep(cep : string): Observable<Cep>{
+    return this.http.get(`${CEP_API}/${cep}/json`)
+      .map(response => response.json())
+      .catch(PrevtransAdminHerrorHandler.handleError);
+  }
+}
---
+++ src/app/prevtrans-admin/services/pessoa.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import {Headers, Http, RequestOptions} from '@angular/http';
+import 'rxjs/add/operator/catch';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/observable/throw';
+import {Observable} from 'rxjs/Observable';
+import {PREVTRANS_API} from '../../app.api';
+import {PrevtransAdminHerrorHandler} from '../prevtrans-admin-herror-handler';
+import {Pessoa} from '../models';
+
+@Injectable()
+export class PessoaService {
+
+  constructor(private http: Http) { }
+
+  pessoas(): Observable<Pessoa[]> {
+    return this.http.get(`${PREVTRANS_API}/pessoas`)
+      .map(response => response.json()).catch(PrevtransAdminHerrorHandler.handleError);
+  }
+  postPessoa( pessoa: Pessoa): Observable<String> {
+    const headers = new Headers();
+    headers.append('Content-Type', 'application/json');
+    return this.http.post(`${PREVTRANS_API}/pessoas`,
+      JSON.stringify(pessoa),
+      new RequestOptions({headers: headers}))
+      .map(response => response.json());
+  }
+}
