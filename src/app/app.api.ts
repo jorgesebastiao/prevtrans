15eef0ff0f0ae380ab97ef9d8f8d2e9b25ae593c
bvjorge <jorgenes1@hotmail.com>
2017-11-23 15:49:57 -0200

    Alterado API.ts

--- src/app/app.api.ts
+++ src/app/app.api.ts
@@ -1,10 +1,10 @@
 // local host
- export const PREVTRANS_API = 'http://localhost:8080/prevtrans-ws/v1';
- export const AUTH_LOGOUT = 'http://localhost:8080/tokens/revoke';
- export const AUTH_LOGIN = 'http://localhost:8080/oauth/token';
+// export const PREVTRANS_API = 'http://localhost:8080/prevtrans-ws/v1';
+// export const AUTH_LOGOUT = 'http://localhost:8080/tokens/revoke';
+// export const AUTH_LOGIN = 'http://localhost:8080/oauth/token';
 // API CEPS
  export const CEP_API = 'http://viacep.com.br/ws/';
 // API PREVTRANS
-// export const PREVTRANS_API = 'http://www.prevtrans.com.br:8080/prevtrans-ws/v1';
-// export const AUTH_LOGIN = 'http://www.prevtrans.com.br:8080/oauth/token';
-// export const AUTH_LOGOUT = 'http://www.prevtrans.com.br:8080/tokens/revoke';
+ export const PREVTRANS_API = 'http://www.prevtrans.com.br:8080/prevtrans-ws/v1';
+ export const AUTH_LOGIN = 'http://www.prevtrans.com.br:8080/oauth/token';
+ export const AUTH_LOGOUT = 'http://www.prevtrans.com.br:8080/tokens/revoke';
